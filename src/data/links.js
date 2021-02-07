d8062a1d9a3f5618f2e129b098384a0a53149e41
Ernestas-wq <ernestas.lakstinis1@gmail.com>
2021-02-07 09:35:29 +0200

    Added portfolio link

--- src/data/links.js
+++ src/data/links.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FaHome } from 'react-icons/fa';
 import { GiNetworkBars } from 'react-icons/gi';
+import { MdWork } from 'react-icons/md';
 
 export const links = [
   {
@@ -14,5 +15,11 @@ export const links = [
     url: '/highscores',
     text: 'highscores',
     icon: <GiNetworkBars />,
+  },
+  {
+    id: 3,
+    url: 'https://ernestas-portfolio.netlify.app/',
+    text: 'portfolio',
+    icon: <MdWork />,
   },
 ];
